2b5da71edfec6c2793c40b2cef7c5355c9db9e5b
Jassar Al-akhras <JAlakhras@blessedtreeit.com>
2019-03-08 21:36:46 +0200

    Add entites class

---
+++ Books.Entities/Entities/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Entities.Entities
+{
+    public enum Genre
+    {
+
+        [Display(Name = "Non Fiction")]
+        NonFiction,
+        Romance,
+        Action,
+        [Display(Name = "Science Fiction")]
+        ScienceFiction
+    }
+}
