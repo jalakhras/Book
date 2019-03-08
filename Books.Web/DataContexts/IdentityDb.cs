d253649b8f150362b948acc12b6d5911ca44b24c
Jassar Al-akhras <JAlakhras@blessedtreeit.com>
2019-03-08 22:05:49 +0200

    Create bookDb and separate identity in a new db

---
+++ Books.Web/DataContexts/IdentityDb.cs
@@ -0,0 +1,38 @@
+﻿using Books.Web.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Books.Web.DataContexts
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+
+    }
+}
+
+
+//public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+//{
+//    public ApplicationDbContext()
+//        : base("DefaultConnection", throwIfV1Schema: false)
+//    {
+//    }
+
+//    public static ApplicationDbContext Create()
+//    {
+//        return new ApplicationDbContext();
+//    }
+//}
